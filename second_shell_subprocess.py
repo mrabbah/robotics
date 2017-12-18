9be814e3a45bc9fe13dcfdb2c34bae8f0875bfe5
RABBAH Mahmoud <mrabbah@gmail.com>
2017-12-18 07:57:51 +0000

    new tests
    new tests

--- second_shell_subprocess.py
+++ second_shell_subprocess.py
@@ -8,11 +8,16 @@ received = False
 
 
 def signal_usr1(signum, frame):
-    "Callback invoked when a signal is received"
+    "Callback invoked when a signal is received %d" % signum
     global received
     received = True
-    print('CHILD {:>6}: Received USR1'.format(pid))
-    sys.stdout.flush()
+    if signum == signal.SIGUSR1:
+    	print('CHILD {:>6}: Received USR1'.format(pid))
+    	sys.stdout.flush()
+    else:
+    	print('CHILD {:>6}: Im not concerned with this signal'.format(pid))
+    	sys.stdout.flush()
+    	signal.pause()
 
 
 print('CHILD {:>6}: Setting up signal handler'.format(pid))
@@ -20,10 +25,11 @@ sys.stdout.flush()
 signal.signal(signal.SIGUSR1, signal_usr1)
 print('CHILD {:>6}: Pausing to wait for signal'.format(pid))
 sys.stdout.flush()
-time.sleep(15)
+signal.pause()
+#time.sleep(15)
 
-if not received:
-    print('CHILD {:>6}: Never received signal'.format(pid))
+#if not received:
+#    print('CHILD {:>6}: Never received signal'.format(pid))
 
-print('End of subprocess ...')
+print('CHILD {:>6}: End of subprocess ...'.format(pid))
 sys.stdout.flush()
